e9ee107bca48ee5540b5f43c889e59a5d6d677c9
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-03-21 01:11:07 +0800

    Entits

---
+++ flexi.Entities/Entities/Venue.cs
@@ -0,0 +1,9 @@
+namespace flexi.Entities
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+        public string VenueName { get; set; } = string.Empty;
+        public int VenueCapacity { get; set; }
+    }
+}
