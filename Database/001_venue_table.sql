f3dfb7d83ba72eef23e0215e54b2316c5725dfd5
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-03-21 21:37:53 +0800

    separated sql scripts

--- Database/001_venue_table.sql
+++ Database/001_venue_table.sql
@@ -1,71 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 21, 2025 at 05:14 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+CREATE TABLE venue (
+  venueId int(11) NOT NULL,
+  venueName varchar(255) NOT NULL,
+  venueCapacity int(11) NOT NULL
+) 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ALTER TABLE venue
+  ADD PRIMARY KEY (venueId);
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `flexi_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `venue`
---
-
-CREATE TABLE `venue` (
-  `venueId` int(11) NOT NULL,
-  `venueName` varchar(255) NOT NULL,
-  `venueCapacity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `venue`
---
-
-INSERT INTO `venue` (`venueId`, `venueName`, `venueCapacity`) VALUES
-(1, 'CRMC Quadrangle', 400),
-(2, 'CRMC Quadrangle', 0),
-(3, 'AVR', 200),
-(4, 'AVR', 100),
-(5, 'Maxwell', 200),
-(6, 'CRMC Pandan Campus GYM', 1000);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `venue`
---
-ALTER TABLE `venue`
-  ADD PRIMARY KEY (`venueId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `venue`
---
-ALTER TABLE `venue`
-  MODIFY `venueId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE venue 
+  MODIFY COLUMN venueId INT(11) NOT NULL AUTO_INCREMENT;
---
+++ Database/002_insert_into_venue.sql
@@ -0,0 +1,8 @@
+INSERT INTO venue (venueId, venueName, venueCapacity) 
+VALUES
+(1, 'CRMC Quadrangle', 400),
+(2, 'CRMC Quadrangle', 0),
+(3, 'AVR', 200),
+(4, 'AVR', 100),
+(5, 'Maxwell', 200),
+(6, 'CRMC Pandan Campus GYM', 1000);
