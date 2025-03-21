6d6f195d7ea81a21eb424d4b2fcf76e3adc76d27
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-03-21 21:41:12 +0800

    make venue repo a file scoped namespace

--- flexi.Repository/Implementations/VenueRepository.cs
+++ flexi.Repository/Implementations/VenueRepository.cs
@@ -2,35 +2,33 @@ using Dapper;
 using flexi.Entities;
 using TechTalk.DatabaseAccessor.Services;
 
-namespace flexi.Repository
+namespace flexi.Repository;
+public class VenueRepository : IVenueRepository
 {
-    public class VenueRepository : IVenueRepository
+    private readonly IDatabaseAccessor _databaseAccessor;
+
+    private const string tableName = "venue";
+
+    public VenueRepository(IDatabaseAccessor databaseAccessor)
     {
-        private readonly IDatabaseAccessor _databaseAccessor;
+        _databaseAccessor = databaseAccessor;
+    }
 
-        private const string tableName = "venue";
+    public async Task<IEnumerable<Venue>> GetAllVenues()
+    {
+        string sql = $"SELECT * FROM {tableName}";
+        return await _databaseAccessor.QueryAsync<Venue>(sql);
+    }
 
-        public VenueRepository(IDatabaseAccessor databaseAccessor)
-        {
-            _databaseAccessor = databaseAccessor;
-        }
-
-        public async Task<IEnumerable<Venue>> GetAllVenues()
-        {
-            string sql = $"SELECT * FROM {tableName}";
-            return await _databaseAccessor.QueryAsync<Venue>(sql);
-        }
-
-        public async Task<Venue> AddVenue(Venue venueInfo)
-        {
-            string sql = $@"
+    public async Task<Venue> AddVenue(Venue venueInfo)
+    {
+        string sql = $@"
             INSERT INTO {tableName} (VenueName, VenueCapacity) 
             VALUES (@VenueName, @VenueCapacity);
-            SELECT LAST_INSERT_ID();"; 
+            SELECT LAST_INSERT_ID();";
 
-            int newId = await _databaseAccessor.InsertScalarAsync<int>(sql, venueInfo);
-            venueInfo.VenueId = newId;
-            return venueInfo;
-        }
+        int newId = await _databaseAccessor.InsertScalarAsync<int>(sql, venueInfo);
+        venueInfo.VenueId = newId;
+        return venueInfo;
     }
 }
